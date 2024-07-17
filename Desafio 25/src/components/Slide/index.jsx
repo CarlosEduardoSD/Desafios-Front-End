bfccc114e9662025a6fb33de114b9761e5be9e14
Carlos Eduardo <carloseduardosilva5858@gmail.com>
2024-07-17 20:15:39 -0300

    feat: owl.carousel in slide component

--- Desafio 25/src/components/Slide/index.jsx
+++ Desafio 25/src/components/Slide/index.jsx
@@ -1,76 +1,54 @@
 import styles from "./styles.module.css";
 
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
 import LeBron from "../../assets/images/LeBronSlide.png";
 import Kobe from "../../assets/images/KobeSlide.png";
 
 export default function Slide() {
+  const options = {
+    loop: true,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 1000,
+    navSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  };
+
   return (
     <>
       <section id={styles.slide_section}>
         <div className="container">
-          <div id="carouselExample" className="carousel slide" data-bs-interval="5000">
-            <div className="carousel-inner">
-              <div
-                className="carousel-item active"
-                style={{ display: "flex", justifyContent: "space-evenly" }}
-              >
-                <div className={styles.slide}>
-                  <img src={LeBron} alt="LeBron James" />
-                  <p>
-                    Aos 39 anos, LeBron James atingiu a marca de 40 mil pontos
-                    na temporada regular da NBA
-                  </p>
-                </div>
-                <div className={styles.slide}>
-                  <img src={Kobe} alt="Kobe Bryant" />
-                  <p>
-                    Morte de Kobe Bryant: piloto de helicóptero ficou
-                    desorientado com mau tempo
-                  </p>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className={styles.slide}>
-                  <img src={Kobe} alt="Kobe Bryant" />
-                  <p>
-                    Morte de Kobe Bryant: piloto de helicóptero ficou
-                    desorientado com mau tempo
-                  </p>
-                </div>
-                <div className={styles.slide}>
-                  <img src={LeBron} alt="LeBron James" />
-                  <p>
-                    Aos 39 anos, LeBron James atingiu a marca de 40 mil pontos
-                    na temporada regular da NBA
-                  </p>
-                </div>
-              </div>
+          <OwlCarousel className="owl-theme" {...options}>
+            <div className={styles.slide}>
+              <img src={LeBron} alt="LeBron James" />
+              <p>
+                Aos 39 anos, LeBron James atingiu a marca de 40 mil pontos na
+                temporada regular da NBA
+              </p>
             </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExample"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExample"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
+            <div className={styles.slide}>
+              <img src={Kobe} alt="Kobe Bryant" />
+              <p>
+                Morte de Kobe Bryant: piloto de helicóptero ficou desorientado
+                com mau tempo
+              </p>
+            </div>
+          </OwlCarousel>
         </div>
       </section>
     </>
